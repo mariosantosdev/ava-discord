e3d682912f9139597d32bb209894d80ed0a3402f
nevr001 <playcar46@gmail.com>
2020-12-27 23:11:27 -0300

    Create interface
    Interface of model props of a guild

--- src/type/interface.ts
+++ src/type/interface.ts
@@ -5,3 +5,34 @@ export interface RunEvent {
     bot: Client,
     args: string[]
 }
+
+export interface GuildProps {
+    /**
+     * The Unique ID of the guild
+     */
+    id: number,
+    /**
+     * The name of the guild
+     */
+    name?: string,
+    /**
+     * The full amount of members in this guild
+     */
+    memberCount?: number,
+    /**
+     * The region the guild is located in
+     */
+    region?: string,
+    /**
+     * The timestamp the client user joined the guild at
+     */
+    joined?: number
+    /**
+     * The user ID of this guild's owner
+     */
+    ownerID?: string,
+    /**
+     * If bot has been removed from the guild
+     */
+    deleted?: boolean,
+}
