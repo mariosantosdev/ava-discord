f61874759a5cf494bb07fa2f26494717ba2a826e
nevr001 <playcar46@gmail.com>
2020-12-27 21:00:45 -0300

    Created function handler command
    That bot use handler commands on path src/commands

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,33 @@
 import 'dotenv/config'
 import { Client, Collection } from 'discord.js'
+import { readdirSync } from 'fs'
+import path from 'path'
+
+import { RunEvent } from '@type/interface'
+
 const bot = new Client()
+const commands: Collection<string[], (event: RunEvent) => any> = new Collection()
 
 // Emit a message in console when bot is ready
 bot.on('ready', () => console.log('[core] => bot start done'))
+
+// Handle commands file
+try {
+    // Read all files in path './commands'
+    const commandsFiles = readdirSync(path.resolve(__dirname, 'commands'))
+
+    // If not find any file command throw Error
+    if (commandsFiles.length <= 0) throw new Error('File not find in path commands')
+
+    commandsFiles.forEach(file => {
+        // Import espefic command file
+        const command: { names: string[], run: (event: RunEvent) => any } = require(`./commands/${file}`)
+
+        // Set a command in Collection
+        commands.set(command.names, command.run)
+    })
+} catch (error) {
+    console.log(error);
+}
 bot.login(process.env.TOKEN)
     .catch(console.error)
