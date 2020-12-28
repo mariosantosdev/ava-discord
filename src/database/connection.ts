49fec977490f2b080ca955c87b86c6620de0a7b2
nevr001 <playcar46@gmail.com>
2020-12-27 23:09:05 -0300

    Create connection with database

---
+++ src/database/connection.ts
@@ -0,0 +1,17 @@
+import { createConnection } from 'typeorm'
+import path from 'path'
+
+export default createConnection({
+    name: "sqlite",
+    type: "sqlite",
+    database: path.resolve(__dirname, "database.sqlite"),
+    synchronize: true,
+    logging: false,
+    entities: [
+        __dirname + "\\..\\models\\**\\*.ts"
+    ],
+    cli: {
+        entitiesDir:  __dirname + "\\..\\models\\**\\*.ts"
+    }
+})
+    .then(() => console.log('[database] -> create done'))
