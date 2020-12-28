d4cc4837fabdbd185880ea54eed99e827b6af5db
nevr001 <playcar46@gmail.com>
2020-12-27 21:05:01 -0300

    Created temp db channels
    This db is temporary to store channels of commands and chat

---
+++ src/utils/Channel.ts
@@ -0,0 +1,14 @@
+export default {
+    /**
+     * @string
+     * Id of the channel commands in discord
+     * FIRST ID GO BE USED FOR REDIRECT MESSAGE
+     */
+    "commads": [],
+    /**
+     * @string
+     * Id of the channel chat in discord
+     * FIRST ID GO BE USED FOR REDIRECT MESSAGE
+     */
+    "chat": []
+}
