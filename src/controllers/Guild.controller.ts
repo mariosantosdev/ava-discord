6e2bab6329c1377d7027eec7b94ef19bb79f4049
nevr001 <playcar46@gmail.com>
2020-12-27 23:12:29 -0300

    Create controller guild
    this controller has the basic to store a guild on database

---
+++ src/controllers/Guild.controller.ts
@@ -0,0 +1,71 @@
+import { getConnection } from 'typeorm'
+import Guild from '@models/Guild'
+import { GuildProps } from '@type/interface'
+
+export default function GuildController() {
+    // Find guild on DATABASE
+    async function findGuild(id: number) {
+        try {
+            // SELECT guild from ID using getConnection with name `sqlite` and 
+            const guild = await getConnection('sqlite')
+                .createQueryBuilder()
+                .select('guild')
+                .from(Guild, "guild")
+                .where("guild.id = :id", { id })
+                .getOneOrFail()
+
+            return guild
+        } catch (error) {
+            // If guild not found on DATABASE
+            return false
+        }
+    }
+
+    async function addGuild(values: GuildProps) {
+        // Check if guild exists on DATABASE
+        if (await findGuild(values.id)) {
+            // Update state of guild on DATABASE
+            return updateGuild({ id: values.id, deleted: false })
+        }
+
+        try {
+            // Insert new guild using getConnection with name `sqlite`
+            await getConnection('sqlite')
+                .createQueryBuilder()
+                .insert()
+                .into(Guild)
+                .values(values)
+                .execute()
+        } catch (error) {
+            console.log(error);
+
+            return false
+        }
+    }
+
+    async function updateGuild(values: GuildProps) {
+        // Check if guild exists on DATABASE
+        if (await !findGuild(values.id)) return
+
+        try {
+            await getConnection('sqlite')
+                .createQueryBuilder()
+                .update(Guild)
+                .set({ ...values })
+                .where("id = :id", {id: values.id})
+                .execute()
+                .then(console.log)
+                .catch(console.error)
+        } catch (error) {
+            console.log(error);
+
+            return false
+        }
+    }
+
+    return {
+        findGuild,
+        addGuild,
+        updateGuild
+    }
+}
