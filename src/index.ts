2903ce8cb7e5d9e97ef1c7e218cf838eb97a8559
nevr001 <playcar46@gmail.com>
2020-12-27 21:08:11 -0300

    Created function to execute command
    Using the command from a command message

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,8 @@ import path from 'path'
 
 import { RunEvent } from '@type/interface'
 
+import CheckWhereMessageWasSent from '@utils/Message'
+
 const bot = new Client()
 const commands: Collection<string[], (event: RunEvent) => any> = new Collection()
 
@@ -29,5 +31,33 @@ try {
 } catch (error) {
     console.log(error);
 }
+
+// Event message discord.js
+bot.on('message', async message => {
+    // Get all args of message
+    const args = message.content.split(' ')
+    if (args.length < 1) return
+
+    // Check if message is a command
+    const isCommand = args[0].split('')[0] === process.env.PREFIX ? true : false
+
+    // Check the channel of message
+    CheckWhereMessageWasSent({ message, bot, args })
+
+    if (isCommand) {
+        if (message.author.bot) return
+
+        // Get name command
+        const command = args.shift()!.slice(process.env.PREFIX?.length);
+        
+        // Find in commands a command with key of above 
+        const commandFile = commands.find((value, key) => key.includes(command));
+        if (!commandFile) return
+
+        // If found a command execute it
+        commandFile && commandFile({ message, bot, args })
+    }
+})
+
 bot.login(process.env.TOKEN)
     .catch(console.error)
