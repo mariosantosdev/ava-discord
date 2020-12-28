63138d9a3f6fecf24331a920e580a78304dc6b9c
nevr001 <playcar46@gmail.com>
2020-12-27 23:10:59 -0300

    Create model of database
    This is a model of guild from discord

---
+++ src/models/Guild.ts
@@ -0,0 +1,37 @@
+import { Entity, Column, PrimaryColumn } from 'typeorm'
+
+@Entity()
+export default class Guild {
+    @PrimaryColumn({ type: 'numeric', unique: true })
+    id: number
+
+    @Column('varchar')
+    name: string
+
+    @Column({ type: 'varchar', default: '$' })
+    prefix: string
+
+    @Column({ type: 'simple-array', nullable: true })
+    channels_command: []
+
+    @Column({ type: 'simple-array', nullable: true })
+    channels_chat: []
+
+    @Column({ type: 'simple-array', nullable: true })
+    prefix_redirect: []
+
+    @Column('numeric')
+    memberCount: number
+
+    @Column('varchar')
+    region: string
+
+    @Column('varchar')
+    ownerID: string
+
+    @Column('boolean')
+    deleted: boolean
+
+    @Column('varchar')
+    joined: number
+}
