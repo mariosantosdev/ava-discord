4468c58d14e6efbdab9a344e163fbaa026517be7
nevr001 <playcar46@gmail.com>
2020-12-28 17:00:53 -0300

    Created file types response

---
+++ src/type/response.ts
@@ -0,0 +1,8 @@
+/**
+ * Interface response when find a specific field
+ */
+export interface ResponseSelectField {
+    prefix_redirect?: string[] | []
+    channels_chat?: string[] | []
+    channels_command?: string[] | []
+}
