1a6b67a6c14913d209ce79ce235c24f78245d30c
nevr001 <playcar46@gmail.com>
2020-12-29 17:42:06 -0300

    Remove force type in Get Fields from file removeChannel

--- src/commands/removeChannel.ts
+++ src/commands/removeChannel.ts
@@ -49,7 +49,7 @@ export async function run(event: RunEvent) {
     // If type channel is a chat
     if (event.args[0] === 'chat') {
         // Get current channels of guild
-        const { channels_chat }: { channels_chat: string[] } = await GuildController().selectField(guildID, ["channels_chat"])
+        const { channels_chat } = await GuildController().selectField(guildID, ["channels_chat"])
         // If not get channels chat
         if (!channels_chat) return event.message.reply('Desculpe, não consegui analisar seus canais atual')
 
@@ -69,7 +69,7 @@ export async function run(event: RunEvent) {
 
     } else if (event.args[0] === 'command') {
         // Get current channels of guild
-        const { channels_command }: { channels_command: string[] } = await GuildController().selectField(guildID, ["channels_command"])
+        const { channels_command } = await GuildController().selectField(guildID, ["channels_command"])
         // If not get channels chat
         if (!channels_command) return event.message.reply('Desculpe, não consegui analisar seus canais atual')
 
