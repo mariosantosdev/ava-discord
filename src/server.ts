f6ff4c019ef64a9856d6786a4e5df683329dc031
nevr001 <playcar46@gmail.com>
2020-12-30 17:09:31 -0300

    Add console log when start server

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,9 @@ app.use('/', (req, res) => {
     return res.status(200).send('Hello, I\'m the AvaBOT')
 })
 
-app.listen(process.env.PORT || 3333)
+app.listen(process.env.PORT || 3333, () => {
+    console.log('[server] => server start done')
+})
 
 
 export default app
