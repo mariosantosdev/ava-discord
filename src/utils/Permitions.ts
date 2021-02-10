732e84261e6cad821dbdac472a6fbb1d060c5010
nevr001 <playcar46@gmail.com>
2021-02-10 15:04:35 -0300

    Created new permitions

--- src/utils/Permitions.ts
+++ src/utils/Permitions.ts
@@ -3,3 +3,5 @@ import { GuildMember } from "discord.js";
 export const ADMIN = (member: GuildMember) => member.hasPermission('ADMINISTRATOR')
 export const BAN_MEMBER = (member: GuildMember) => member.hasPermission('BAN_MEMBERS')
 export const KICK_MEMBER = (member: GuildMember) => member.hasPermission('KICK_MEMBERS')
+export const MANAGE_CHANNELS = (member: GuildMember) => member.hasPermission('MANAGE_CHANNELS')
+export const MANAGE_GUILD = (member: GuildMember) => member.hasPermission('MANAGE_GUILD')
